97788ce06484672470aba6cc6dd63712eb6c10d6
Patsy <patsyesaurer@gmail.com>
2024-01-26 22:26:22 -0800

    updated the footer font colors

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className="footer">
       <footer className="p-15 d-block text-center text-grey">
       <p style={{color:"#EE05F2"}}>&copy; Patsy Saurer <span style={{color:"#2F1D73"}}> | </span><span style={{color:"#6451A6"}}>{new Date().getFullYear()}</span></p>
-        <p style={{ fontSize: "0.7rem" }}>
-          <span style={{ color: "#2F1D73" }}>P.S. </span>
+        <p style={{ fontSize: "0.7rem", color:"#6451A6" }}>
+          <span style={{ color: "#EE05F2" }}>P.S. </span>
           <a
             style={{ textDecoration: "none", color: "#F088F2" }}
             href="https://www.cursors-4u.com/cursor/2013/04/16/starbucks-strawberries-cr-me-frappuccino.html"
