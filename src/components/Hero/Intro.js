ae71584a714e598d5d69266f5076bc33d71ffd8b
Patsy <patsyesaurer@gmail.com>
2024-01-26 22:04:46 -0800

    added JavScript to Tech Stack section

--- src/components/Hero/Intro.js
+++ src/components/Hero/Intro.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./Hero.css";
 
 const Intro = () => {
   return (
     <div id="intro-section">
     <div className="text-content">
-        <h3>Hello! I'm</h3>
+        <h3 className='hello'>Hello! I'm</h3>
         <h1>Patsy Saurer</h1>
         <h3>A full-stack developer</h3>
         <br />
--- src/components/Skills/SkillsCard.js
+++ src/components/Skills/SkillsCard.js
@@ -2,12 +2,19 @@ import React from "react";
 import { FaReact } from "react-icons/fa";
 import { DiRuby } from "react-icons/di";
 import { SiRubyonrails } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+
 import "./Skills.css";
 
 const SkillsCard = () => {
   return (
     <>
       <div className="skills-group">
+      <div className="skills-card">
+          <IoLogoJavascript style={{color: "hotpink", fontSize: "3.5rem"}}/>
+          <h4>JavaScript</h4>
+          <p>Front-End Language</p>
+        </div>
         <div className="skills-card">
           <FaReact style={{color: "hotpink", fontSize: "3.5rem"}}/>
           <h4>React</h4>
@@ -23,6 +30,8 @@ const SkillsCard = () => {
           <h4>Ruby on Rails</h4>
           <p>Back-End Framework</p>
         </div>
+
+        
       </div>
     </>
   );
