46e98fcb86622a4aa28511f7d2aa6c66197ef8c8
Patsy <patsyesaurer@gmail.com>
2024-01-26 22:08:25 -0800

    updated JavaScript icon to a rounded icon

--- src/components/Skills/SkillsCard.js
+++ src/components/Skills/SkillsCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FaReact } from "react-icons/fa";
 import { DiRuby } from "react-icons/di";
 import { SiRubyonrails } from "react-icons/si";
-import { IoLogoJavascript } from "react-icons/io5";
+import { RiJavascriptFill } from "react-icons/ri";
 
 import "./Skills.css";
 
@@ -10,28 +10,26 @@ const SkillsCard = () => {
   return (
     <>
       <div className="skills-group">
-      <div className="skills-card">
-          <IoLogoJavascript style={{color: "hotpink", fontSize: "3.5rem"}}/>
+        <div className="skills-card">
+          <RiJavascriptFill style={{ color: "hotpink", fontSize: "3.5rem" }} />
           <h4>JavaScript</h4>
           <p>Front-End Language</p>
         </div>
         <div className="skills-card">
-          <FaReact style={{color: "hotpink", fontSize: "3.5rem"}}/>
+          <FaReact style={{ color: "hotpink", fontSize: "3.5rem" }} />
           <h4>React</h4>
           <p>Front-End Framework</p>
         </div>
         <div className="skills-card">
-          <DiRuby style={{color: "hotpink", fontSize: "3.5rem"}}/>
+          <DiRuby style={{ color: "hotpink", fontSize: "3.5rem" }} />
           <h4>Ruby</h4>
           <p>Back-End Language</p>
         </div>
         <div className="skills-card">
-          <SiRubyonrails style={{color: "hotpink", fontSize: "3.5rem"}}/>
+          <SiRubyonrails style={{ color: "hotpink", fontSize: "3.5rem" }} />
           <h4>Ruby on Rails</h4>
           <p>Back-End Framework</p>
         </div>
-
-        
       </div>
     </>
   );
