9f175e8c29125b4a8f980d3a6fe09bf8708d25e7
Patsy <patsyesaurer@gmail.com>
2024-01-26 16:42:03 -0800

    added footer credits

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
-        <footer className="p-10 d-block text-center text-grey">
-        &copy; Patsy Saurer | {new Date().getFullYear()}
+      <footer className="p-15 d-block text-center text-grey">
+      <p style={{color:"#EE05F2"}}>&copy; Patsy Saurer <span style={{color:"#2F1D73"}}> | </span><span style={{color:"#6451A6"}}>{new Date().getFullYear()}</span></p>
+        <p style={{ fontSize: "0.7rem" }}>
+          <span style={{ color: "#2F1D73" }}>P.S. </span>
+          <a
+            style={{ textDecoration: "none", color: "#F088F2" }}
+            href="https://www.cursors-4u.com/cursor/2013/04/16/starbucks-strawberries-cr-me-frappuccino.html"
+          >
+             starbucks strawberries and cream frappuccino cursor
+          </a>{" "}
+          relates to my barista days as a sbux ☕️ partner
+        </p>
+        <a style={{fontSize:"0.6rem", textDecoration:"none", color:"#6451A6"}}
+          href="https://www.flaticon.com/free-icons/sparkle"
+          title="sparkle icons"
+        >
+          Sparkle icons created by Freepik
+        </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
