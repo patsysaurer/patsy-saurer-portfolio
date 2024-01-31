d7951f1ea38be6e234a76299d9c670f45269fbf3
Patsy <patsyesaurer@gmail.com>
2024-01-30 19:04:59 -0800

    changed about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className='about' id='about'>
       <div className='about-text'>
         <h3>About Me</h3>
-        <p>Hello! I’m Patsy, a developer with a sweet twist. My experience is a combination of chocolate and baking with the discipline of military service. Quality, time-management, and commitment are my top three values. And just like chocolate and pastry, code can become very complex with a sweet reward. Challenging myself is what I thrive on. I’m excited to see how I can improve the pastry world using software development.</p>
+        <p>Hello! I’m Patsy, a developer with a sweet twist. My experience is a combination of chocolate and baking with the discipline of military service. Quality, time-management, and commitment are my top three values. And just like chocolate and pastry, code can become very complex with a sweet reward. Challenging myself is what I thrive on.</p>
       </div>
     </div>
   )
